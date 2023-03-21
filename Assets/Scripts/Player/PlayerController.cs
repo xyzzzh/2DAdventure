68606d3ab8f5f2aea95e10f8336ec5a6914cf9d3
ZhuXiuyuan <zhuxiuyuan@lumverse.com>
2023-03-21 13:13:39 +0800

    实现人物移动、翻转和跳跃

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,21 @@ public class PlayerController : MonoBehaviour
 {
     public PlayerInputControl inputControl;
     public Vector2 inputDirection;
+    private Rigidbody2D rb;
+
+    [Header("Basic Params")]
+    public float speed;
+    public float jumpForce;
+    
 
     //启动顺序：Awake()->OnEnable()->Start()
     private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         inputControl = new PlayerInputControl();
+
+        // 注册函数：+=
+        inputControl.GamePlay.Jump.started += jump;
     }
 
     private void OnEnable()
@@ -34,5 +45,27 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+    }
+
+    public void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+        // 翻转人物
+        int faceDir = (int)transform.localScale.x;
+
+        if (inputDirection.x > 0) faceDir = 1;
+        if (inputDirection.x < 0) faceDir = -1;
+
+        transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+
+    private void jump(InputAction.CallbackContext obj)
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 }

03a2cff470c50d5085d4001ae3fbeee9f9d9b303
ZhuXiuyuan <zhuxiuyuan@lumverse.com>
2023-03-21 09:24:06 +0800

    添加项目文件。

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    public Vector2 inputDirection;
+
+    //Æô¶¯Ë³Ðò£ºAwake()->OnEnable()->Start()
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+    }
+}

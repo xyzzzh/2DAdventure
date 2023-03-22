04e7752efcde065145653b60af02a077ceec8d07
Chamelz <972395684@qq.com>
2023-03-22 22:07:02 +0800

    增加Idle Run动画状态机

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetAnimation();
+    }
+
+    public void SetAnimation()
+    {
+        anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
+    }
+}

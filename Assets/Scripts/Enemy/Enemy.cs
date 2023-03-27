eff5bba078dd4b55d0d3c119d04887b00e5ed89f
xyzzzh <972395684@qq.com>
2023-03-27 20:27:55 +0800

    为野猪boar添加受伤及死亡动画。学习了协程和动画关键帧中调用事件。

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -13,10 +14,16 @@ public class Enemy : MonoBehaviour
     public float chaseSpeed;
     public float currSpeed;
     public Vector3 faceDir;
+    public float hurtForce;
 
     [Header("计时器")] public float waitTime;
     public float waitTimeCounter;
     public bool wait;
+
+    public Transform attacker;
+
+    [Header("受伤状态")] public bool isHurt;
+    public bool isDead;
     
     private void Awake()
     {
@@ -41,7 +48,10 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        if (!isHurt)
+        {
+            Move();
+        }
     }
 
     public virtual void Move()
@@ -61,5 +71,46 @@ public class Enemy : MonoBehaviour
                 transform.localScale = new Vector3(faceDir.x, 1, 1);
             }
         }
+    }
+
+    public void OnTakeDamage(Transform attackerTrans)
+    {
+        attacker = attackerTrans;
+        // 转身
+        if (attacker.position.x - transform.position.x > 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (attacker.position.x - transform.position.x < 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        
+        //受伤被击退
+        isHurt = true;
+        anim.SetTrigger("hurt");
+        Vector2 dir = new Vector2(transform.position.x - attacker.position.x, 0);
+        //调用协程
+        StartCoroutine(OnHurt(dir));
+    }
+
+    // 协程
+    private IEnumerator OnHurt(Vector2 dir)
+    {
+        rb.AddForce(dir*hurtForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.45f);
+        isHurt = false;
+    }
+
+    public void OnDie()
+    {
+        this.gameObject.layer = 2;
+        anim.SetBool("dead", true);
+        isDead = true;
+    }
+
+    public void DestroyAfterAnimation()
+    {
+        Destroy(this.gameObject);
     }
 }
